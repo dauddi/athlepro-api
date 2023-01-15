66c3d3c5f3b08d61c0834c0ff4105b92549b68af
Davis Okioma <davis.okioma@savannahinformatics.com>
2023-01-15 21:43:44 +0300

    feat: inial controllers

---
+++ src/controllers/profiles.controller.mjs
@@ -0,0 +1,33 @@
+import Profile from "../models/profiles.model.mjs";
+
+const getAllProfiles = async (req, res) => {
+	try {
+		const profiles = await Profile.fetchAll();
+		res.status(200).json(profiles);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const addProfile = async (req, res) => {
+	try {
+		const profile = new Profile(req.body);
+		const result = await profile.save();
+		res.status(201).json(result);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const updateProfile = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const profile = new Profile({ ...req.body, id });
+		const result = await profile.updateProfile();
+		res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+export { getAllProfiles, addProfile, updateProfile };
